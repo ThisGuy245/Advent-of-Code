af0824aeb3b21c8449693fc88fe4bada41bfd313
Thommy <150126564+ThisGuy245@users.noreply.github.com>
2026-02-19 04:46:23 +0000

    Part 2 complete

--- 2025/2025_day01/2025_day01.cpp
+++ 2025/2025_day01/2025_day01.cpp
@@ -9,50 +9,76 @@
     https://adventofcode.com/2025/day/1
 */
 
+
 // Dial 0 - 99, starts at 50
 // attached document 
 // L +  pos  or R + pos
 // example: dial = 11 , R8 -> deal = 19
 // example: dial = 19 , L20 -> deal = 99
 // we only count the amount of times it has pointed to 0
-
-int main()
-{
-    std::cout << "Let's begin!\n" << std::endl;
-
+int part1(const std::vector<std::string>& rotations) {
     int dial = 50;
     int actual = 0;
-    std::ifstream input("input.txt");
-    std::string line;
-
-    // Error handling
-    if (!input.is_open())
-    {
-        std::cerr << "Error: Impossible to open file!" << std::endl;
-        return 1;
-    }
-
-    // Logic
-    while (std::getline(input, line))
-    {
-        char direction = line[0]; // Gets me first letter [L/R]
-        int clicks = std::stoi(line.substr(1)); // Makes the rest of the line into an int [13..]
+    for (const auto& line : rotations) {
+        char direction = line[0];
+        int clicks = std::stoi(line.substr(1));
 
         if (direction == 'L') {
-            dial = (dial - clicks + 100) % 100; // Modulo always keeps +ve
+            dial = (dial - clicks + 100) % 100;
         }
         else if (direction == 'R') {
             dial = (dial + clicks) % 100;
         }
         if (dial == 0) {
-            actual += 1;
+            actual++;
         }
     }
-
-    input.close();
-    std::cout << "The dial hit '0', " << actual << " times!" << std::endl;
-
+    return actual;
 }
 
+// This time the number of ANY click that passes 0 
+// is registered as the actual amount
+int part2(const std::vector<std::string>& rotations) {
+    int dial = 50;
+    int actual = 0;
+    for (const auto& line : rotations) {
+        char direction = line[0];
+        int clicks = std::stoi(line.substr(1));
 
+        for (int i = 0; i < clicks; ++i) {
+            dial = (direction == 'L') ? (dial - 1 + 100) % 100 : (dial + 1) % 100;
+            if (dial == 0) {
+                actual++;
+            }
+        }
+    }
+    return actual;
+}
 
+int main() {
+    std::cout << "Let's begin!\n" << std::endl;
+
+    // Read the file 'example or input'
+    std::ifstream input("input.txt");
+    if (!input.is_open()) {
+        std::cerr << "Error: Impossible to open file!" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::string> rotations;
+    std::string line;
+    while (std::getline(input, line)) {
+        rotations.push_back(line);
+    }
+    input.close();
+
+    // Part 1
+    int resultPart1 = part1(rotations);
+    std::cout << "Part 1: The dial hit '0', " << resultPart1 << " times!" << std::endl;
+
+    // Part 2
+    int resultPart2 = part2(rotations);
+    std::cout << "Part 2: The dial hit '0', " << resultPart2 << " times!" << std::endl;
+
+    return 0;
+}
